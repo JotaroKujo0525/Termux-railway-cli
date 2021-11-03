e1b5d7904d75662d0179c941c471b3b8dc99514a
JotaroKujo0525 <77835007+JotaroKujo0525@users.noreply.github.com>
2021-11-03 11:12:57 +0800

    Update railway.sh

--- railway.sh
+++ railway.sh
@@ -1,3 +1,4 @@
-apt install sudo
-sudo apt install curl
+apt install wget -y
+apt install sudo -y
+sudo apt install curl -y
 sh -c "$(curl -sSL https://raw.githubusercontent.com/railwayapp/cli/master/install.sh)" ##install railway-cli 
