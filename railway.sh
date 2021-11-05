cb178fe52c8eca23956e033a813a2220e5291016
root <cloudandreibacor@gmail.com>
2021-11-05 03:57:22 +0000

    Updating

--- railway.sh
+++ railway.sh
@@ -5,16 +5,14 @@ echo
 echo
 echo
 echo -e "Also please note that ${RED}you need to b
-e rooted ${NC}to use this method. Refer to the ${R
-ED}README.md file of the repository for a guide"
+e rooted ${NC}to use this method. Refer to the ${RED}README.md file of the repository for a guide"
 echo
 echo -e "This will install ${RED}Railway Cli ${NC}
 in your cellphone. Sit back and relax while the sc
 ript do the installing :)"
 echo
 echo -e "Having issues? Try contacting me on my di
-scord account ${RED}(Jotaro Kujo#0525) ${NC}or ${R
-ED}join my support server here! ${NC} https://disc
+scord account ${RED}(Jotaro Kujo#0525) ${NC}or ${RED}join my support server here! ${NC} https://disc
 ord.gg/cgjW7Xr2ns"
 echo
 
